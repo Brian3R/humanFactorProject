d9652db48e83abf4f8bcd940ff4e29574c04b612
Owen Ziegler <odz2@txstate.edu>
2023-04-02 16:54:59 -0500

    Navigation Bar Skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 import {useState, useEffect} from "react";
 //function change
 import { getTest } from "./functions/test";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './components/Home';
+import AddItem from './components/AddItem';
+import Generator from './components/Generator';
+import Inventory from './components/Inventory';
 
 function App() {
+  /*
   const [data, setData] = useState('Hello World!');
 
   useEffect(() =>{
@@ -20,7 +26,17 @@ function App() {
       <h1>{data}</h1>
      
     </div>
-  );
+  );*/
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/additem" element={<AddItem/>}/>
+        <Route path="/generator" element={<Generator/>}/>
+        <Route path="/inventory" element={<Inventory/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ client/src/components/AddItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const AddItem = () => {
+    return(
+        <div>
+            <Navbar/>
+            <h1>
+                Add Item!
+            </h1>
+        </div>
+    );
+}
+
+export default AddItem;
