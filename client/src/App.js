2bc90f98d2ca7590ee4f84277e08a99ea453051f
Brian Robinson <bwr37@txstate.edu>
2023-04-03 16:14:47 -0500

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
-import {useState, useEffect} from "react";
+
 //function change
-import { getTest } from "./functions/test";
+
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './components/Home';
 import AddItem from './components/AddItem';
@@ -10,23 +10,7 @@ import Generator from './components/Generator';
 import Inventory from './components/Inventory';
 
 function App() {
-  /*
-  const [data, setData] = useState('Hello World!');
 
-  useEffect(() =>{
-    getTest()
-    .then((res) => {
-      setData(res.message);
-    })
-    .catch((err) => console.log(err));
-  }, []);
-
-  return (
-    <div className="App">
-      <h1>{data}</h1>
-     
-    </div>
-  );*/
   return(
     <BrowserRouter>
       <Routes>
