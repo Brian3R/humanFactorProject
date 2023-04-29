c9399bb8b27d97a78df106454c7a5b97218ddd1b
Brian Robinson <bwr37@txstate.edu>
2023-04-29 17:57:28 -0500

    star

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ app.use(cors({origin: true, credentials: true}));
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   }); 
 app.get('/', (req,res) =>{
